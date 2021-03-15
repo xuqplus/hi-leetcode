881f1be3329b4525d2b9d525d07dd842aebbdca1
Qunqun Xu <xuqunqun@live.cn>
2021-03-15 22:12:19 +0800

    Leetcode 0007

---
+++ src/test/java/com/github/xuqplus/hi/leetcode/q0007/ATest.java
@@ -0,0 +1,34 @@
+package com.github.xuqplus.hi.leetcode.q0007;
+
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.Test;
+
+/**
+ * https://leetcode-cn.com/problems/two-sum/
+ */
+@Slf4j
+public class ATest {
+
+    @Test
+    void a() {
+        Solution solution = new Solution();
+        int target = -2147483648;
+        log.info("{}", solution.reverse(target));
+    }
+}
+
+class Solution {
+    public int reverse(int x) {
+        if (0 == x || Integer.MIN_VALUE == x) {
+            return 0;
+        }
+        if (0 < x) {
+            try {
+                return Integer.valueOf(new StringBuilder(String.valueOf(x)).reverse().toString());
+            } catch (NumberFormatException e) {
+                return 0;
+            }
+        }
+        return -reverse(-x);
+    }
+}
