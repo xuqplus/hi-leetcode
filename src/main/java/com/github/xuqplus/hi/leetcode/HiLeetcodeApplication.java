cbf10b71a795d2ecc2af16c79b2844ee64957a8a
Qunqun Xu <xuqunqun@live.cn>
2021-03-15 21:35:26 +0800

    Initialize with leetcode q0001

---
+++ src/main/java/com/github/xuqplus/hi/leetcode/HiLeetcodeApplication.java
@@ -0,0 +1,13 @@
+package com.github.xuqplus.hi.leetcode;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HiLeetcodeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HiLeetcodeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/github/xuqplus/hi/leetcode/HiLeetcodeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.xuqplus.hi.leetcode;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HiLeetcodeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
