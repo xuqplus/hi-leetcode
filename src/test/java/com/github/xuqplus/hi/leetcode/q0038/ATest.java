218bff7123abda804dfd16993786c1267f5ac97a
Qunqun Xu <xuqunqun@live.cn>
2021-03-20 00:01:51 +0800

    Q0038 count and say

--- src/test/java/com/github/xuqplus/hi/leetcode/q0038/ATest.java
+++ src/test/java/com/github/xuqplus/hi/leetcode/q0038/ATest.java
@@ -14,12 +14,38 @@ public class ATest {
     @Test
     void a() {
         Solution solution = new Solution();
-        log.info("{}", solution.run());
+        log.info("{}", solution.countAndSay(1));
+        log.info("{}", solution.countAndSay(2));
+        log.info("{}", solution.countAndSay(3));
+        log.info("{}", solution.countAndSay(4));
+        log.info("{}", solution.countAndSay(5));
+        log.info("{}", solution.countAndSay(6));
+        log.info("{}", solution.countAndSay(30));
     }
 }
 
 class Solution {
-    public int run() {
-        return 0;
+    public String countAndSay(int n) {
+        String curr = "1";
+        for (int i = 1; i < n; i++) {
+            curr = next(curr);
+        }
+        return curr;
+    }
+
+    static String next(String n) {
+        final int length = n.length();
+        int curr = 1, currCount = 1;
+        StringBuilder sb = new StringBuilder();
+        for (; curr < length; curr++) {
+            if (n.charAt(curr) == n.charAt(curr - 1)) {
+                currCount++;
+            } else {
+                sb.append(String.format("%s%s", currCount, n.charAt(curr - 1)));
+                currCount = 1;
+            }
+        }
+        sb.append(String.format("%s%s", currCount, n.charAt(curr - 1)));
+        return sb.toString();
     }
 }
