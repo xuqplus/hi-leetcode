0b0b1ce29ae55dcb2c3c3a5fd3359861c034e80b
Qunqun Xu <xuqunqun@live.cn>
2021-03-20 17:21:11 +0800

    Q0009 palindrome number

--- src/test/java/com/github/xuqplus/hi/leetcode/q0009/ATest.java
+++ src/test/java/com/github/xuqplus/hi/leetcode/q0009/ATest.java
@@ -3,6 +3,9 @@ package com.github.xuqplus.hi.leetcode.q0009;
 import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * 回文数
  * easy
@@ -14,12 +17,25 @@ public class ATest {
     @Test
     void a() {
         Solution solution = new Solution();
-        log.info("{}", solution.run());
+        log.info("{}", solution.isPalindrome(1234321));
     }
 }
 
 class Solution {
-    public int run() {
-        return 0;
+    public boolean isPalindrome(int x) {
+        if (x < 0) {
+            return false;
+        }
+        List<Integer> nums = new ArrayList<>();
+        nums.add(x % 10);
+        while ((x /= 10) > 0) {
+            nums.add(x % 10);
+        }
+        for (int i = 0; i <= nums.size() / 2; i++) {
+            if (nums.get(i) != nums.get(nums.size() - i - 1)) {
+                return false;
+            }
+        }
+        return true;
     }
 }
