e2324ae5b5d47b9d3b9731e7b6b05a6209743612
Qunqun Xu <xuqunqun@live.cn>
2021-03-22 23:31:08 +0800

    Q0066 - plus one

--- src/test/java/com/github/xuqplus/hi/leetcode/q0066/ATest.java
+++ src/test/java/com/github/xuqplus/hi/leetcode/q0066/ATest.java
@@ -14,12 +14,37 @@ public class ATest {
     @Test
     void a() {
         Solution solution = new Solution();
-        log.info("{}", solution.run());
+        log.info("{}", solution.plusOne(new int[]{1, 2, 3}));
+        log.info("{}", solution.plusOne(new int[]{9, 9, 9}));
     }
 }
 
 class Solution {
-    public int run() {
-        return 0;
+    public int[] plusOne(int[] digits) {
+        for (int i = digits.length - 1; i >= 0; i--) {
+            boolean up = plusOne(digits, i);
+            if (up) {
+                if (0 == i) {
+                    int[] result = new int[digits.length + 1];
+                    result[0] = 1;
+                    System.arraycopy(digits, 0, result, 1, digits.length);
+                    return result;
+                }
+            } else {
+                break;
+            }
+        }
+        return digits;
+    }
+
+    private boolean plusOne(int[] digits, int i) {
+        int n = digits[i];
+        if (n < 9) {
+            digits[i] = n + 1;
+            return false;
+        } else {
+            digits[i] = 0;
+            return true;
+        }
     }
 }
