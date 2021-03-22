2f9451b1c8dd94c50f8c4a1ea343287d71323a9a
Qunqun Xu <xuqunqun@live.cn>
2021-03-23 00:36:55 +0800

    Q0088 - merge sorted array

--- src/test/java/com/github/xuqplus/hi/leetcode/q0067/ATest.java
+++ src/test/java/com/github/xuqplus/hi/leetcode/q0067/ATest.java
@@ -14,12 +14,61 @@ public class ATest {
     @Test
     void a() {
         Solution solution = new Solution();
-        log.info("{}", solution.run());
+        log.info("{}", solution.addBinary("11", "1"));
     }
 }
 
 class Solution {
-    public int run() {
-        return 0;
+    public String addBinary(String a, String b) {
+        final String x = a.length() > b.length() ? a : b;
+        final String y = a.length() <= b.length() ? a : b;
+        final int max = x.length();
+        final int min = y.length();
+        final StringBuilder sb = new StringBuilder();
+        int i = max - 1;
+        boolean up = false;
+        for (; i >= max - min; i--) {
+            if (up) {
+                up = add(sb, x.charAt(i), y.charAt(i + min - max), true);
+            } else {
+                up = add(sb, x.charAt(i), y.charAt(min + i - max));
+            }
+        }
+        for (; i >= 0; i--) {
+            if (up) {
+                up = add(sb, x.charAt(i), '0', true);
+            } else {
+                up = add(sb, x.charAt(i), '0');
+            }
+        }
+        if (up) {
+            sb.insert(0, '1');
+            return sb.toString();
+        }
+        return sb.toString();
+    }
+
+    private boolean add(StringBuilder sb, char a, char b, boolean up) {
+        if ('1' == a) {
+            sb.insert(0, b);
+        } else if ('1' == b) {
+            sb.insert(0, a);
+        } else {
+            sb.insert(0, '1');
+            return false;
+        }
+        return true;
+    }
+
+    static boolean add(StringBuilder sb, char a, char b) {
+        if ('0' == a) {
+            sb.insert(0, b);
+        } else if ('0' == b) {
+            sb.insert(0, a);
+        } else {
+            sb.insert(0, '0');
+            return true;
+        }
+        return false;
     }
 }
--- src/test/java/com/github/xuqplus/hi/leetcode/q0088/ATest.java
+++ src/test/java/com/github/xuqplus/hi/leetcode/q0088/ATest.java
@@ -3,6 +3,8 @@ package com.github.xuqplus.hi.leetcode.q0088;
 import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
 
+import java.util.Arrays;
+
 /**
  * 合并两个有序数组
  * easy
@@ -14,12 +16,18 @@ public class ATest {
     @Test
     void a() {
         Solution solution = new Solution();
-        log.info("{}", solution.run());
+        int[] ints = {2, 0};
+        solution.merge(ints, 1, new int[]{1}, 1);
+        log.info("{}", ints);
     }
 }
 
 class Solution {
-    public int run() {
-        return 0;
+    public void merge(int[] nums1, int m, int[] nums2, int n) {
+        if (null == nums2 || 0 == nums2.length) {
+            return;
+        }
+        System.arraycopy(nums2, 0, nums1, m, n);
+        Arrays.sort(nums1);
     }
 }
