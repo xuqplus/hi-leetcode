feccad0e11e682f0fe4d05ff02f92bef69f738f6
Qunqun Xu <qunqun.xu@autodesk.com>
2021-03-22 12:59:28 +0800

    Q0069 sqrt - dichotomy

--- src/test/java/com/github/xuqplus/hi/leetcode/q0069/ATest.java
+++ src/test/java/com/github/xuqplus/hi/leetcode/q0069/ATest.java
@@ -14,12 +14,31 @@ public class ATest {
     @Test
     void a() {
         Solution solution = new Solution();
-        log.info("{}", solution.run());
+        log.info("{}", solution.mySqrt(2147395599));
     }
 }
 
 class Solution {
-    public int run() {
-        return 0;
+    // dichotomy
+    public int mySqrt(long x) {
+        if (0 >= x) {
+            return 0;
+        }
+        // (0, x];
+        long a = x;
+        long d = x >> 1;
+        long a_2;
+        boolean minus;
+        while ((minus = (a_2 = a * a) > x) || a_2 + a * 2 + 1 <= x) {
+            if (minus) {
+                a -= d;
+            } else {
+                a += d;
+            }
+            d >>= 1;
+            if (0 >= d)
+                d = 1;
+        }
+        return (int) a;
     }
 }
