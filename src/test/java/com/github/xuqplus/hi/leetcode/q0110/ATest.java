025df3c27ecc2cdfdf51a08efe6c5c6722da71f1
Qunqun Xu <xuqunqun@live.cn>
2021-09-18 21:34:33 +0800

    q0110 - balanced binary tree

--- src/test/java/com/github/xuqplus/hi/leetcode/q0110/ATest.java
+++ src/test/java/com/github/xuqplus/hi/leetcode/q0110/ATest.java
@@ -14,12 +14,56 @@ public class ATest {
     @Test
     void a() {
         Solution solution = new Solution();
-        log.info("{}", solution.run());
+        log.info("{}", solution.isBalanced(new TreeNode()));
     }
 }
 
 class Solution {
-    public int run() {
-        return 0;
+    public boolean isBalanced(TreeNode root) {
+        if (null == root) {
+            return true;
+        }
+        return solution(root);
+    }
+
+    static boolean solution(TreeNode node) {
+        boolean[] result = {true};
+        height(node, result);
+        return result[0];
+    }
+
+    static int height(TreeNode node, boolean[] r) {
+        if (!r[0]) {
+            return -1;
+        }
+        if (null == node) {
+            return 0;
+        }
+        int lh = height(node.left, r);
+        int rh = height(node.right, r);
+        if (Math.abs(lh - rh) > 1) {
+            r[0] = false;
+        }
+        return 1 + Math.max(lh, rh);
+    }
+
+}
+
+class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+
+    TreeNode() {
+    }
+
+    TreeNode(int val) {
+        this.val = val;
+    }
+
+    TreeNode(int val, TreeNode left, TreeNode right) {
+        this.val = val;
+        this.left = left;
+        this.right = right;
     }
 }
