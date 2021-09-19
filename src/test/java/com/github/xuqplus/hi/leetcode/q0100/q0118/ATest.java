f552ab756a91f7dd3153a0774825e0e333591c75
Qunqun Xu <xuqunqun@live.cn>
2021-09-19 14:00:23 +0800

    q0118 - pascals triangle

--- src/test/java/com/github/xuqplus/hi/leetcode/q0100/q0118/ATest.java
+++ src/test/java/com/github/xuqplus/hi/leetcode/q0100/q0118/ATest.java
@@ -3,6 +3,8 @@ package com.github.xuqplus.hi.leetcode.q0100.q0118;
 import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
 
+import java.util.*;
+
 /**
  * 杨辉三角
  * easy
@@ -14,12 +16,46 @@ public class ATest {
     @Test
     void a() {
         Solution solution = new Solution();
-        log.info("{}", solution.run());
+        log.info("{}", solution.generate(5));
     }
 }
 
 class Solution {
-    public int run() {
-        return 0;
+
+    static Map<Integer, List<Integer>> map = new HashMap<>(1 << 6);
+
+    static {
+        List<Integer> firstRow = Arrays.asList(1);
+        map.put(0, firstRow);
+    }
+
+    static int sum(List<Integer> arr, int i0, int i1) {
+        if (i0 < 0 || i1 >= arr.size()) {
+            return 1;
+        }
+        return arr.get(i0) + arr.get(i1);
+    }
+
+    public List<List<Integer>> generate(int numRows) {
+        if (numRows <= 0) {
+            return null;
+        }
+        List<List<Integer>> result = new ArrayList<>();
+        for (int i = 0; i < numRows; i++) {
+            List<Integer> cache = map.get(i);
+            if (null != cache) {
+                result.add(cache);
+                continue;
+            }
+            List<Integer> previous = result.get(i - 1);
+            List<Integer> current = new ArrayList<>();
+            for (int j = 0; j < i + 1; j++) {
+                int sum = sum(previous, j - 1, j);
+                current.add(sum);
+            }
+            map.put(i, current);
+            result.add(current);
+        }
+        return result;
     }
 }
