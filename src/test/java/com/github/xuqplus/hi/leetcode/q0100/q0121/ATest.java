81b778449c97bfde6a1be78dbaf4f75825c305a9
Qunqun Xu <xuqunqun@live.cn>
2021-09-19 14:34:52 +0800

    q0121 - best time to buy and sell stock

--- src/test/java/com/github/xuqplus/hi/leetcode/q0100/q0121/ATest.java
+++ src/test/java/com/github/xuqplus/hi/leetcode/q0100/q0121/ATest.java
@@ -14,12 +14,45 @@ public class ATest {
     @Test
     void a() {
         Solution solution = new Solution();
-        log.info("{}", solution.run());
+        log.info("{}", solution.maxProfit(new int[]{1}));
     }
 }
 
 class Solution {
-    public int run() {
-        return 0;
+
+    // timed out
+    static int solution1(int[] prices) {
+        if (null == prices || prices.length <= 1) {
+            return 0;
+        }
+        int result = 0;
+        for (int i = 0; i < prices.length - 1; i++) {
+            for (int j = i + 1; j < prices.length; j++) {
+                result = Math.max(result, prices[j] - prices[i]);
+            }
+        }
+        return result;
+    }
+
+    static int solution2(int[] prices) {
+        if (null == prices || prices.length <= 1) {
+            return 0;
+        }
+        int valley = prices[0], peak = prices[0];
+        int result = 0;
+        for (int price : prices) {
+            if (price < valley) {
+                valley = price;
+                peak = price;
+            } else if (price > peak) {
+                peak = price;
+                result = Math.max(result, peak - valley);
+            }
+        }
+        return result;
+    }
+
+    public int maxProfit(int[] prices) {
+        return solution2(prices);
     }
 }
