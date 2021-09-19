c23fb373edc7cbfc9de4e53f6f967927e2ff0288
Qunqun Xu <xuqunqun@live.cn>
2021-09-19 15:12:11 +0800

    q0125 - valid palindrome with special characters

--- src/test/java/com/github/xuqplus/hi/leetcode/q0100/q0125/ATest.java
+++ src/test/java/com/github/xuqplus/hi/leetcode/q0100/q0125/ATest.java
@@ -3,6 +3,8 @@ package com.github.xuqplus.hi.leetcode.q0100.q0125;
 import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
 
+import java.util.Objects;
+
 /**
  * 验证回文串
  * easy
@@ -14,12 +16,52 @@ public class ATest {
     @Test
     void a() {
         Solution solution = new Solution();
-        log.info("{}", solution.run());
+        log.info("{}", solution.isPalindrome("A man, a plan, a canal: Panama"));
     }
 }
 
 class Solution {
-    public int run() {
-        return 0;
+
+    static int a = 'a';
+    static int z = 'z';
+    static int A = 'A';
+    static int Z = 'Z';
+    static int zero = '0';
+    static int nine = '9';
+
+    static boolean isIncluded(char c) {
+        if (a <= c && c <= z) {
+            return true;
+        }
+        if (A <= c && c <= Z) {
+            return true;
+        }
+        if (zero <= c && c <= nine) {
+            return true;
+        }
+        return false;
+    }
+
+    public boolean isPalindrome(String s) {
+        if (Objects.isNull(s)) {
+            return false;
+        }
+        for (int i = 0, j = s.length() - 1; i < j; i++) {
+            char ci = s.charAt(i);
+            if (isIncluded(ci)) {
+                for (; i < j; j--) {
+                    char cj = s.charAt(j);
+                    if (isIncluded(cj)) {
+                        if (ci == cj || Character.toLowerCase(ci) == Character.toLowerCase(cj)) {
+                            j--;
+                            break;
+                        } else {
+                            return false;
+                        }
+                    }
+                }
+            }
+        }
+        return true;
     }
 }
