a53a8a62de779978d60a9c320dcc0d77c2a0feae
Qunqun Xu <xuqunqun@live.cn>
2021-09-20 16:05:19 +0800

    q0155 - stack

--- src/test/java/com/github/xuqplus/hi/leetcode/q0100/q0155/ATest.java
+++ src/test/java/com/github/xuqplus/hi/leetcode/q0100/q0155/ATest.java
@@ -3,6 +3,8 @@ package com.github.xuqplus.hi.leetcode.q0100.q0155;
 import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
 
+import java.util.Stack;
+
 /**
  * 最小栈
  * easy
@@ -13,13 +15,39 @@ public class ATest {
 
     @Test
     void a() {
-        Solution solution = new Solution();
-        log.info("{}", solution.run());
+        MinStack solution = new MinStack();
+        log.info("{}", solution);
     }
 }
 
-class Solution {
-    public int run() {
-        return 0;
+class MinStack {
+
+    final Stack<Integer> stack = new Stack();
+
+    /**
+     * initialize your data structure here.
+     */
+    public MinStack() {
+
+    }
+
+    public void push(int val) {
+        stack.push(val);
+    }
+
+    public void pop() {
+        stack.pop();
+    }
+
+    public int top() {
+        return stack.lastElement();
+    }
+
+    public int getMin() {
+        int r = Integer.MAX_VALUE;
+        for (Integer v : stack) {
+            r = Math.min(r, v);
+        }
+        return r;
     }
 }
