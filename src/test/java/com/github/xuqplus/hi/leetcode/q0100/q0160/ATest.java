b5aacca4751e9a6cf5f6b611b06042c6aa28116f
Qunqun Xu <xuqunqun@live.cn>
2021-09-20 22:38:06 +0800

    q0160 - intersect lists - optimized

--- src/test/java/com/github/xuqplus/hi/leetcode/q0100/q0160/ATest.java
+++ src/test/java/com/github/xuqplus/hi/leetcode/q0100/q0160/ATest.java
@@ -1,7 +1,11 @@
 package com.github.xuqplus.hi.leetcode.q0100.q0160;
 
+import com.github.xuqplus.hi.leetcode.common.ListNode;
 import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
+
+import java.util.HashSet;
+import java.util.Set;
 
 /**
  * 相交链表
@@ -14,12 +18,45 @@ public class ATest {
     @Test
     void a() {
         Solution solution = new Solution();
-        log.info("{}", solution.run());
+        ListNode headA = new ListNode(1);
+        log.info("{}", solution.getIntersectionNode(headA, headA));
     }
 }
 
 class Solution {
-    public int run() {
-        return 0;
+
+    static ListNode solution(ListNode headA, ListNode headB) {
+        if (null == headA || null == headB) {
+            return null;
+        }
+        Set<ListNode> set = new HashSet<>();
+        while (null != headA) {
+            set.add(headA);
+            headA = headA.next;
+        }
+        while (null != headB) {
+            if (set.contains(headB)) {
+                return headB;
+            }
+            set.add(headB);
+            headB = headB.next;
+        }
+        return null;
+    }
+
+    static ListNode solution2(ListNode headA, ListNode headB) {
+        if (null == headA || null == headB) {
+            return null;
+        }
+        ListNode a = headA, b = headB;
+        while (a != b) {
+            a = null == a ? headB : a.next;
+            b = null == b ? headA : b.next;
+        }
+        return a;
+    }
+
+    public ListNode getIntersectionNode(ListNode headA, ListNode headB) {
+        return solution2(headA, headB);
     }
 }
