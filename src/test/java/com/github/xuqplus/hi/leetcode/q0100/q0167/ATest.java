fc4faf69900ee173a7e8c6d9ac3c3a7b25cbedb9
Qunqun Xu <xuqunqun@live.cn>
2021-09-20 23:42:58 +0800

    q0167 - two sum ii

--- src/test/java/com/github/xuqplus/hi/leetcode/q0100/q0167/ATest.java
+++ src/test/java/com/github/xuqplus/hi/leetcode/q0100/q0167/ATest.java
@@ -3,6 +3,9 @@ package com.github.xuqplus.hi.leetcode.q0100.q0167;
 import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
 
+import java.util.HashMap;
+import java.util.Map;
+
 /**
  * 两数之和 II - 输入有序数组
  * easy
@@ -14,12 +17,24 @@ public class ATest {
     @Test
     void a() {
         Solution solution = new Solution();
-        log.info("{}", solution.run());
+        log.info("{}", solution.twoSum(new int[]{1, 2, 3}, 4));
     }
 }
 
 class Solution {
-    public int run() {
-        return 0;
+
+    public int[] twoSum(int[] numbers, int target) {
+        Map<Integer, Integer> map = new HashMap<>();
+        for (int i = 0; i < numbers.length; i++) {
+            map.put(numbers[i], i);
+        }
+        for (int i = 0; i < numbers.length; i++) {
+            int k = target - numbers[i];
+            Integer index = map.get(k);
+            if (null != index) {
+                return new int[]{i + 1, index + 1};
+            }
+        }
+        return null;
     }
 }
