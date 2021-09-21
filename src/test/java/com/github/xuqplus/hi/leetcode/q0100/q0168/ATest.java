d284033b833905daf5b6deb545886fa59942e71c
Qunqun Xu <xuqunqun@live.cn>
2021-09-21 10:56:56 +0800

    q0168 - excel title

--- src/test/java/com/github/xuqplus/hi/leetcode/q0100/q0168/ATest.java
+++ src/test/java/com/github/xuqplus/hi/leetcode/q0100/q0168/ATest.java
@@ -14,12 +14,45 @@ public class ATest {
     @Test
     void a() {
         Solution solution = new Solution();
-        log.info("{}", solution.run());
+        for (int i = 0; i < 64; i++) {
+            log.info("{} - {} - {}", i, Integer.toString(i, 'Z' - 'A' + 1), solution.convertToTitle(i));
+        }
     }
 }
 
 class Solution {
-    public int run() {
-        return 0;
+    public String convertToTitle(int columnNumber) {
+        // 'a' 97
+        // 'A' 65
+        // '0' 48
+        String s = Integer.toString(columnNumber, 'Z' - 'A' + 1);
+        // the first letter would not be '0'
+        char[] r = new char[s.length()];
+        for (int i = 1; i < r.length; i++) {
+            char c = s.charAt(i);
+            char prev = s.charAt(i - 1);
+            if ('0' == c) {
+                prev -= 1;
+            }
+            if ('0' <= prev && prev <= '9') {
+                prev += 'A' - '0' - 1;
+            } else {
+                prev += 'A' - 'a' + 10 - 1;
+            }
+            if ('@' != prev)
+                r[i - 1] = prev;
+        }
+        r[r.length - 1] = trans(s.charAt(r.length - 1));
+        return new String(r);
+    }
+
+    static char trans(char c) {
+        if ('0' == c) {
+            return 'Z';
+        }
+        if ('0' < c && c <= '9') {
+            return (char) (c + 'A' - '0' - 1);
+        }
+        return (char) (c + 'A' - 'a' + 10 - 1);
     }
 }
