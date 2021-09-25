e12561e6b9b5a7ff84bc7d3c35da57ca7c50dcfc
xuqplus <xuqunqun@live.cn>
2021-09-25 21:42:03 +0800

    q0190 - int reverse

--- src/test/java/com/github/xuqplus/hi/leetcode/q0100/q0190/ATest.java
+++ src/test/java/com/github/xuqplus/hi/leetcode/q0100/q0190/ATest.java
@@ -14,12 +14,21 @@ public class ATest {
     @Test
     void a() {
         Solution solution = new Solution();
-        log.info("{}", solution.run());
+        log.info("{}", solution.reverseBits(0xffff0000));
+        log.info("{}", solution.reverseBits(0x0000ffff));
     }
 }
 
 class Solution {
-    public int run() {
-        return 0;
+
+    // you need treat n as an unsigned value
+    public int reverseBits(int n) {
+        // return Integer.reverse(n);
+        int r = 0;
+        for (int i = 0; i < 16; i++) {
+            r |= ((0b00000000000000000000000000000001 << i) & n) << (32 - i - i - 1);
+            r |= ((0b10000000000000000000000000000000 >>> i) & n) >>> (32 - i - i - 1);
+        }
+        return r;
     }
 }
