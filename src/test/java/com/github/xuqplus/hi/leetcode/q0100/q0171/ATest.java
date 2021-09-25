eba2d3d13434254a4487f3e1a46655f76f91c77e
xuqplus <xuqunqun@live.cn>
2021-09-25 20:32:11 +0800

    q0171 - excel sheet column number

--- src/test/java/com/github/xuqplus/hi/leetcode/q0100/q0171/ATest.java
+++ src/test/java/com/github/xuqplus/hi/leetcode/q0100/q0171/ATest.java
@@ -14,12 +14,22 @@ public class ATest {
     @Test
     void a() {
         Solution solution = new Solution();
-        log.info("{}", solution.run());
+        log.info("{}", solution.titleToNumber("AA"));
+        log.info("{}", solution.titleToNumber("AB"));
     }
 }
 
 class Solution {
-    public int run() {
-        return 0;
+
+    static final long radix = 26;
+
+    public int titleToNumber(String columnTitle) {
+        long r = 0;
+        byte[] bytes = columnTitle.getBytes();
+        for (int i = 0; i < bytes.length; i++) {
+            long c = Double.valueOf(Math.pow(radix, bytes.length - i - 1)).longValue();
+            r += c * (bytes[i] - '@');
+        }
+        return (int) r;
     }
 }
