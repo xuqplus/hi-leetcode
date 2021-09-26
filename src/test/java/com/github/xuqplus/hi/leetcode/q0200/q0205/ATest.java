1864b0d2be34c556695c04c2119206d5676c16da
xuqplus <xuqunqun@live.cn>
2021-09-26 17:58:56 +0800

    q0205 - isomorphic strings

--- src/test/java/com/github/xuqplus/hi/leetcode/q0200/q0205/ATest.java
+++ src/test/java/com/github/xuqplus/hi/leetcode/q0200/q0205/ATest.java
@@ -14,12 +14,29 @@ public class ATest {
     @Test
     void a() {
         Solution solution = new Solution();
-        log.info("{}", solution.run());
+        log.info("{}", solution.isIsomorphic("foo", "bar"));
     }
 }
 
 class Solution {
-    public int run() {
-        return 0;
+
+    public boolean isIsomorphic(String s, String t) {
+        char[] cs = new char[128];
+        char[] cs2 = new char[128];
+        for (int i = 0; i < s.length(); i++) {
+            char c = s.charAt(i);
+            char c2 = t.charAt(i);
+            if ('\0' == cs[c]) {
+                cs[c] = c2;
+            } else if (cs[c] != c2) {
+                return false;
+            }
+            if ('\0' == cs2[c2]) {
+                cs2[c2] = c;
+            } else if (cs2[c2] != c) {
+                return false;
+            }
+        }
+        return true;
     }
 }
