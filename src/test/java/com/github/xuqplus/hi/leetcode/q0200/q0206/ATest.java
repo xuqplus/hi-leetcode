96f2dbf607c4685a659cca9c40d5c8e045f885ee
xuqplus <xuqunqun@live.cn>
2021-09-27 22:42:11 +0800

    q0206 - reverse list node

--- src/test/java/com/github/xuqplus/hi/leetcode/q0200/q0206/ATest.java
+++ src/test/java/com/github/xuqplus/hi/leetcode/q0200/q0206/ATest.java
@@ -1,5 +1,6 @@
 package com.github.xuqplus.hi.leetcode.q0200.q0206;
 
+import com.github.xuqplus.hi.leetcode.common.ListNode;
 import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
 
@@ -14,12 +15,31 @@ public class ATest {
     @Test
     void a() {
         Solution solution = new Solution();
-        log.info("{}", solution.run());
+        ListNode head = new ListNode(0);
+        ListNode n1 = new ListNode(1);
+        ListNode n2 = new ListNode(2);
+        head.next = n1;
+        n1.next = n2;
+        ListNode r = solution.reverseList(head);
+        log.info("{}", r);
     }
 }
 
 class Solution {
-    public int run() {
-        return 0;
+
+    public ListNode reverseList(ListNode head) {
+        ListNode next = null;
+        while (null != head) {
+            if (null == head.next) {
+                head.next = next;
+                break;
+            }
+            ListNode h = head;
+            ListNode hn = head.next;
+            head.next = next;
+            head = hn;
+            next = h;
+        }
+        return head;
     }
 }
