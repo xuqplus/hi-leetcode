fee242369548c22d5fa7754ba184d57d1204d5ef
xujing.xia@mininglamp.com <xujing.xia@mininglamp.com>
2021-09-29 12:22:39 +0800

    q0226 - reverse binary tree

--- src/test/java/com/github/xuqplus/hi/leetcode/q0200/q0226/ATest.java
+++ src/test/java/com/github/xuqplus/hi/leetcode/q0200/q0226/ATest.java
@@ -1,5 +1,6 @@
 package com.github.xuqplus.hi.leetcode.q0200.q0226;
 
+import com.github.xuqplus.hi.leetcode.common.TreeNode;
 import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
 
@@ -14,12 +15,26 @@ public class ATest {
     @Test
     void a() {
         Solution solution = new Solution();
-        log.info("{}", solution.run());
+        log.info("{}", solution.invertTree(null));
     }
 }
 
 class Solution {
-    public int run() {
-        return 0;
+
+    static void solution(TreeNode node) {
+        if (null == node) {
+            return;
+        }
+        TreeNode left = node.left;
+        TreeNode right = node.right;
+        node.left = right;
+        node.right = left;
+        solution(left);
+        solution(right);
+    }
+
+    public TreeNode invertTree(TreeNode root) {
+        solution(root);
+        return root;
     }
 }
