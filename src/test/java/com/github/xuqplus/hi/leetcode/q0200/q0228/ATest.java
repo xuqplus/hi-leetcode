7230cd38c0b4a93f261c734ab5cd80cf216338a4
xujing.xia@mininglamp.com <xujing.xia@mininglamp.com>
2021-09-29 13:08:23 +0800

    q0228 - summary ranges

--- src/test/java/com/github/xuqplus/hi/leetcode/q0200/q0228/ATest.java
+++ src/test/java/com/github/xuqplus/hi/leetcode/q0200/q0228/ATest.java
@@ -3,6 +3,10 @@ package com.github.xuqplus.hi.leetcode.q0200.q0228;
 import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
 /**
  * 汇总区间
  * easy
@@ -14,12 +18,35 @@ public class ATest {
     @Test
     void a() {
         Solution solution = new Solution();
-        log.info("{}", solution.run());
+        log.info("{}", solution.summaryRanges(new int[]{1, 2, 3, 5}));
     }
 }
 
 class Solution {
-    public int run() {
-        return 0;
+
+    public List<String> summaryRanges(int[] nums) {
+        if (null == nums || nums.length < 1) {
+            return Collections.emptyList();
+        }
+        List<String> r = new ArrayList<>();
+        int count = 0;
+        for (int i = 0; i < nums.length - 1; i++) {
+            if (nums[i + 1] - nums[i] == 1) {
+                count++;
+                continue;
+            }
+            if (0 == count) {
+                r.add(String.valueOf(nums[i]));
+            } else {
+                r.add(nums[i - count] + "->" + nums[i]);
+            }
+            count = 0;
+        }
+        if (0 == count) {
+            r.add(String.valueOf(nums[nums.length - 1]));
+        } else {
+            r.add(nums[nums.length - 1 - count] + "->" + nums[nums.length - 1]);
+        }
+        return r;
     }
 }
