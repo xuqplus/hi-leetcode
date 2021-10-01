af488ea67a859b4e0376721e14e51a90c5f7524f
xuqplus <xuqunqun@live.cn>
2021-10-01 21:45:32 +0800

    q0263 - ugly number

--- src/test/java/com/github/xuqplus/hi/leetcode/q0200/q0263/ATest.java
+++ src/test/java/com/github/xuqplus/hi/leetcode/q0200/q0263/ATest.java
@@ -3,6 +3,9 @@ package com.github.xuqplus.hi.leetcode.q0200.q0263;
 import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
 
+import java.util.LinkedList;
+import java.util.List;
+
 /**
  * 丑数
  * easy
@@ -14,12 +17,35 @@ public class ATest {
     @Test
     void a() {
         Solution solution = new Solution();
-        log.info("{}", solution.run());
+        log.info("{}", solution.isUgly(1));
     }
 }
 
 class Solution {
-    public int run() {
-        return 0;
+
+    static List<Integer> excluded = new LinkedList<>();
+
+    public boolean isUgly(int n) {
+        if (1 == n) {
+            return true;
+        }
+        for (Integer i : excluded) {
+            if (0 == n % i) {
+                return false;
+            }
+        }
+        int t = n;
+        while (0 == n % 2) {
+            n /= 2;
+        }
+        while (0 == n % 3) {
+            n /= 3;
+        }
+        while (0 == n % 5) {
+            n /= 5;
+        }
+        if (1 != n && n > 5)
+            excluded.add(t);
+        return 1 == n;
     }
 }
