d1cdc18962a623025c4f889cd2587bc41e723b4c
xuqplus <xuqunqun@live.cn>
2021-10-01 20:37:23 +0800

    q0258 - add digits

--- src/test/java/com/github/xuqplus/hi/leetcode/q0200/q0258/ATest.java
+++ src/test/java/com/github/xuqplus/hi/leetcode/q0200/q0258/ATest.java
@@ -14,12 +14,22 @@ public class ATest {
     @Test
     void a() {
         Solution solution = new Solution();
-        log.info("{}", solution.run());
+        log.info("{}", solution.addDigits(11));
     }
 }
 
 class Solution {
-    public int run() {
-        return 0;
+
+    public int addDigits(int num) {
+        while (num > 9) {
+            int sum = 0;
+            while (num > 0) {
+                int c = num % 10;
+                sum += c;
+                num /= 10;
+            }
+            num = sum;
+        }
+        return num;
     }
 }
