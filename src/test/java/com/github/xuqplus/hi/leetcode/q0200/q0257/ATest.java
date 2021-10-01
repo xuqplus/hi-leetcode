249b82c0fe409876ef26cef1fac7ca33c86dced1
xuqplus <xuqunqun@live.cn>
2021-10-01 20:31:50 +0800

    q0257 - binary tree paths

--- src/test/java/com/github/xuqplus/hi/leetcode/q0200/q0257/ATest.java
+++ src/test/java/com/github/xuqplus/hi/leetcode/q0200/q0257/ATest.java
@@ -1,7 +1,12 @@
 package com.github.xuqplus.hi.leetcode.q0200.q0257;
 
+import com.github.xuqplus.hi.leetcode.common.TreeNode;
 import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
 
 /**
  * 二叉树的所有路径
@@ -14,12 +19,41 @@ public class ATest {
     @Test
     void a() {
         Solution solution = new Solution();
-        log.info("{}", solution.run());
+        TreeNode root = new TreeNode(0);
+        TreeNode l = new TreeNode(-100);
+        TreeNode l0 = new TreeNode(-200);
+        TreeNode l1 = new TreeNode(-50);
+        TreeNode r = new TreeNode(100);
+        TreeNode r0 = new TreeNode(50);
+        TreeNode r1 = new TreeNode(200);
+        root.left = l;
+        l.left = l0;
+        l.right = l1;
+        root.right = r;
+        r.left = r0;
+        r.right = r1;
+        log.info("{}", solution.binaryTreePaths(root));
     }
 }
 
 class Solution {
-    public int run() {
-        return 0;
+
+    static void solution(TreeNode node, LinkedList<String> paths, List<String> r) {
+        if (null == node) {
+            return;
+        }
+        paths.add(String.valueOf(node.val));
+        if (null == node.left && null == node.right) {
+            r.add(String.join("->", paths));
+        }
+        solution(node.left, paths, r);
+        solution(node.right, paths, r);
+        paths.removeLast();
+    }
+
+    public List<String> binaryTreePaths(TreeNode root) {
+        ArrayList<String> r = new ArrayList<>();
+        solution(root, new LinkedList<>(), r);
+        return r;
     }
 }
