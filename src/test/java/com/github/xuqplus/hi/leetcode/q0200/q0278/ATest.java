3186f0c09658d02dc8bdb446ed9556df29de2154
xuqplus <xuqunqun@live.cn>
2021-10-01 22:03:59 +0800

    q0278 - first bad version

--- src/test/java/com/github/xuqplus/hi/leetcode/q0200/q0278/ATest.java
+++ src/test/java/com/github/xuqplus/hi/leetcode/q0200/q0278/ATest.java
@@ -3,6 +3,9 @@ package com.github.xuqplus.hi.leetcode.q0200.q0278;
 import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
 
+import java.util.Map;
+import java.util.TreeMap;
+
 /**
  * 第一个错误的版本
  * easy
@@ -14,12 +17,46 @@ public class ATest {
     @Test
     void a() {
         Solution solution = new Solution();
-        log.info("{}", solution.run());
+        log.info("{}", solution.firstBadVersion(5));
     }
 }
 
-class Solution {
-    public int run() {
-        return 0;
+class VersionControl {
+    protected boolean isBadVersion(int v) {
+        if (v >= 4) {
+            return true;
+        }
+        return false;
+    }
+}
+
+class Solution extends VersionControl {
+
+    public int firstBadVersion(int n) {
+        Map<Long, Boolean> map = new TreeMap<>();
+        if (wrappedIsBadVersion(map, 1L)) {
+            return 1;
+        }
+        long l = 1, r = n, t = n;
+        while (true) {
+            if (wrappedIsBadVersion(map, t)) {
+                if (!wrappedIsBadVersion(map, t - 1)) {
+                    return (int) t;
+                }
+                r = t;
+            } else {
+                l = t;
+            }
+            t = (l + r) / 2;
+        }
+    }
+
+    boolean wrappedIsBadVersion(Map<Long, Boolean> map, Long n) {
+        if (map.containsKey(n)) {
+            return map.get(n);
+        }
+        boolean version = isBadVersion(n.intValue());
+        map.put(n, version);
+        return version;
     }
 }
