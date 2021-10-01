83d58ac13f92347372f6966c4a172f7659db11d9
xuqplus <xuqunqun@live.cn>
2021-10-01 20:01:16 +0800

    q0235 - the lowest common ancestor of BST

--- src/test/java/com/github/xuqplus/hi/leetcode/q0200/q0235/ATest.java
+++ src/test/java/com/github/xuqplus/hi/leetcode/q0200/q0235/ATest.java
@@ -1,5 +1,6 @@
 package com.github.xuqplus.hi.leetcode.q0200.q0235;
 
+import com.github.xuqplus.hi.leetcode.common.TreeNode;
 import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
 
@@ -14,12 +15,48 @@ public class ATest {
     @Test
     void a() {
         Solution solution = new Solution();
-        log.info("{}", solution.run());
+        TreeNode root = new TreeNode(0);
+        TreeNode l = new TreeNode(-100);
+        TreeNode l0 = new TreeNode(-200);
+        TreeNode l1 = new TreeNode(-50);
+        TreeNode r = new TreeNode(100);
+        TreeNode r0 = new TreeNode(50);
+        TreeNode r1 = new TreeNode(200);
+        root.left = l;
+        l.left = l0;
+        l.right = l1;
+        root.right = r;
+        r.left = r0;
+        r.right = r1;
+        log.info("{}", solution.lowestCommonAncestor(root, l0, l1).val);
     }
 }
 
 class Solution {
-    public int run() {
-        return 0;
+
+    static int solution(TreeNode node, TreeNode p, TreeNode q, TreeNode[] r) {
+        if (null != r[0]) {
+            return 0;
+        }
+        if (null == node) {
+            return 0;
+        }
+        int count = 0;
+        if (p == node || q == node) {
+            count = 1;
+        }
+        int a = solution(node.left, p, q, r);
+        int b = solution(node.right, p, q, r);
+        count += a + b;
+        if (null == r[0] && count > 1) {
+            r[0] = node;
+        }
+        return count;
+    }
+
+    public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+        TreeNode[] r = new TreeNode[1];
+        solution(root, p, q, r);
+        return r[0];
     }
 }
