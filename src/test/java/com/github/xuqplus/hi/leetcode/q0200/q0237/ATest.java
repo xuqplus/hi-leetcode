63dd0677d3b3477375123c4ef9761c4c44374b6a
xuqplus <xuqunqun@live.cn>
2021-10-01 20:10:24 +0800

    q0237 - delete node in a singly linked list

--- src/test/java/com/github/xuqplus/hi/leetcode/q0200/q0237/ATest.java
+++ src/test/java/com/github/xuqplus/hi/leetcode/q0200/q0237/ATest.java
@@ -1,5 +1,6 @@
 package com.github.xuqplus.hi.leetcode.q0200.q0237;
 
+import com.github.xuqplus.hi.leetcode.common.ListNode;
 import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
 
@@ -14,12 +15,23 @@ public class ATest {
     @Test
     void a() {
         Solution solution = new Solution();
-        log.info("{}", solution.run());
+        ListNode head = new ListNode(1);
+        ListNode n0 = new ListNode(1);
+        ListNode n1 = new ListNode(1);
+        head.next = n0;
+        n0.next = n1;
+        solution.deleteNode(n0);
     }
 }
 
 class Solution {
-    public int run() {
-        return 0;
+
+    public void deleteNode(ListNode node) {
+        while (null != node.next.next) {
+            node.val = node.next.val;
+            node = node.next;
+        }
+        node.val = node.next.val;
+        node.next = null;
     }
 }
