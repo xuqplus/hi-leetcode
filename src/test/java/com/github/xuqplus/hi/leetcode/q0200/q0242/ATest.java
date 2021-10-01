4f787625c3ce1deece315f3fb0141054f953a08a
xuqplus <xuqunqun@live.cn>
2021-10-01 20:18:03 +0800

    q0242 - valid anagram

--- src/test/java/com/github/xuqplus/hi/leetcode/q0200/q0242/ATest.java
+++ src/test/java/com/github/xuqplus/hi/leetcode/q0200/q0242/ATest.java
@@ -14,12 +14,28 @@ public class ATest {
     @Test
     void a() {
         Solution solution = new Solution();
-        log.info("{}", solution.run());
+        log.info("{}", solution.isAnagram("bba", "abb"));
     }
 }
 
 class Solution {
-    public int run() {
-        return 0;
+
+    public boolean isAnagram(String s, String t) {
+        char[] cs0 = s.toCharArray();
+        char[] cs1 = t.toCharArray();
+        int[] c0 = new int[128];
+        int[] c1 = new int[128];
+        for (char c : cs0) {
+            c0[c] += 1;
+        }
+        for (char c : cs1) {
+            c1[c] += 1;
+        }
+        for (int i = 0; i < 128; i++) {
+            if (c0[i] != c1[i]) {
+                return false;
+            }
+        }
+        return true;
     }
 }
