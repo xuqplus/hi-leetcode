631a65fb0aa90a548a63610b8b8ae7d53b6c44ac
xuqplus <xuqunqun@live.cn>
2022-04-02 22:23:10 +0800

    q0326 - power of 3

--- src/test/java/com/github/xuqplus/hi/leetcode/q0300/q0326/ATest.java
+++ src/test/java/com/github/xuqplus/hi/leetcode/q0300/q0326/ATest.java
@@ -3,6 +3,9 @@ package com.github.xuqplus.hi.leetcode.q0300.q0326;
 import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
 
+import java.util.HashSet;
+import java.util.Set;
+
 /**
  * 3的幂
  * easy
@@ -14,12 +17,23 @@ public class ATest {
     @Test
     void a() {
         Solution solution = new Solution();
-        log.info("{}", solution.run());
+        log.info("{}", solution.isPowerOfThree(1));
     }
 }
 
 class Solution {
-    public int run() {
-        return 0;
+
+    static Set<Integer> PO3 = new HashSet<>();
+
+    static {
+        int n = 1;
+        while (n > 0) {
+            PO3.add(n);
+            n = (int) (3 * n);
+        }
+    }
+
+    public boolean isPowerOfThree(int n) {
+        return PO3.contains(n);
     }
 }
